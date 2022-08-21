58d2c57d0788b71c6547c7ecdb8636baca90f5c5
hasindu sithmin <hasindusithmin64@gmail.com>
2022-08-21 17:00:35 +0000

    Updates

---
+++ precache-manifest.347289cd921761a8e5d4794e4b9cd5be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84fec30ac129bb4d7612e29f67313493",
+    "url": "/crypto-ideas/index.html"
+  },
+  {
+    "revision": "05ab027417b7f7ef5599",
+    "url": "/crypto-ideas/static/css/main.cd9374a0.chunk.css"
+  },
+  {
+    "revision": "2a21dcf30532f5248eec",
+    "url": "/crypto-ideas/static/js/2.06636e8a.chunk.js"
+  },
+  {
+    "revision": "05ab027417b7f7ef5599",
+    "url": "/crypto-ideas/static/js/main.09d06d74.chunk.js"
+  },
+  {
+    "revision": "dcf54e91ea7db25e6cb1",
+    "url": "/crypto-ideas/static/js/runtime~main.fdad2c8a.js"
+  }
+]);
